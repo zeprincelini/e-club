784197c8042a8c50c42af486878a628277b6b766
zeprincelini <princelini4@gmail.com>
2022-03-25 18:45:41 +0100

    added login and register controllers & routes

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -10,11 +10,21 @@ const signUp = async (req, res) => {
     if (!hash) {
       return res.status(403).json({ error: "error creating account" });
     }
+    const checkEmail = await pool.query(
+      "SELECT * FROM user_account WHERE email = ($1)",
+      [email]
+    );
+    if (checkEmail.rows.length !== 0) {
+      return res.status(401).json({ error: "email already exists" });
+    }
     const query =
       "INSERT INTO user_account (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [firstName, lastName, email, hash];
     const data = await pool.query(query, values);
-    return res.status(200).json({ status: "success", data: data.rows });
+    return res
+      .status(200)
+      .json({ status: "success", data: data.rows })
+      .render("pages/home/index", { token: accessToken });
   } catch (err) {
     return res.status(401).json({ error: err.message });
   }
@@ -26,13 +36,28 @@ const signIn = async (req, res) => {
   const value = [email];
   try {
     const user = await pool.query(query, value);
-    if (!user) {
+    const data = user.rows;
+    if (data[0] === 0) {
       return res
         .status(404)
         .json({ status: "error", message: "email does not exist" });
     }
-    // const isPasswordValid = await bcrypt.compare(password)
-    return res.status(200).json({ user });
+    const isPasswordValid = await bcrypt.compare(password, data[0].password);
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ status: "error", message: "email or password incorrect" });
+    }
+    const fullName = [data[0].first_name, data[0].last_name].join(" ");
+    const token = jwt.sign(
+      { email: data[0].email, name: fullName },
+      process.env.SECRET
+    );
+    return res.status(200).json({
+      status: "success",
+      message: "sign in successful",
+      accessToken: token,
+    });
   } catch (err) {
     return res
       .status(401)
