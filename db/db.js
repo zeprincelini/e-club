d7686bbbe697c55018713ad15a0ef4b3608e2216
zeprincelini <princelini4@gmail.com>
2022-03-29 04:17:41 +0100

    added conString pg

--- db/db.js
+++ db/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  user: process.env.USER,
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString,
 });
 
 pool
